796d8e251171764c67f186f018d846c9fc311886
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-09-17 16:31:37 +0200

    Creating UseNav and ThemeChanger

--- src/components/ThemeChanger/ThemeChanger.jsx
+++ src/components/ThemeChanger/ThemeChanger.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useTheme } from "next-themes";
+import { useState } from "react";
+import Select from "react-select";
+
+import { customThemeStyles, themes } from "../../services/selectedFilterStyles";
+import css from "./ThemeChanger.module.css";
 
 const ThemeChanger = () => {
-  return (
-    <div>ThemeChanger</div>
-  )
-}
+  const { theme, setTheme } = useTheme();
+  const [selectedThemeOption, setSelectedThemeOption] = useState(null);
 
-export default ThemeChanger
+  const handleChange = (selectedOption) => {
+    setSelectedThemeOption(selectedOption);
+    setTheme(selectedOption.value);
+  };
+
+  return (
+    <Select
+      value={selectedThemeOption}
+      defaultValue={selectedThemeOption}
+      onChange={handleChange}
+      options={themes}
+      className={css.select}
+      styles={customThemeStyles}
+      placeholder={theme}
+    />
+  );
+};
+
+export default ThemeChanger;
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
 
-const UserMenue = () => {
+import { selectAuthUser } from "../../redux/auth/selectors";
+import { logoutUser } from "../../redux/auth/operations";
+import scrollController from "../../services/noScroll";
+import { closeByEsc } from "../../services/functions";
+import css from "./UserMenu.module.css";
+import sprite from "../../assets/sprite.svg";
+
+const UserMenu = () => {
+  const dispatch = useDispatch();
+  const userData = useSelector(selectAuthUser);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openMenu() {
+    setIsOpen(true);
+    scrollController.disabledScroll();
+  }
+
+  function closeMenu() {
+    setIsOpen(false);
+    scrollController.enabledScroll();
+  }
+
+  useEffect(() => {
+    closeByEsc(closeMenu);
+  }, []);
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+  };
+
   return (
-    <div>UserMenue</div>
-  )
-}
+    <>
+      <div className={css.container}>
+        <p className={css.text}>
+          Hello,&nbsp;<span className={css.name}>{userData.name}!</span>
+        </p>
+        <button className={css.button} onClick={handleLogout} type="button">
+          Logout
+        </button>
+      </div>
 
-export default UserMenue
+      <div className={css.containerMob}>
+        <button className={css.buttonMob} onClick={openMenu} type="button">
+          <svg className={css.iconLogOut} width="20" height="20">
+            <use href={`${sprite}#icon-log-out`}></use>
+          </svg>
+        </button>
+
+        {isOpen && <div className={css.backdrop} onClick={closeMenu}></div>}
+
+        {isOpen && (
+          <div className={css.logOutMobContainer}>
+            <button
+              type="button"
+              className={css.btnLogOutMob}
+              onClick={handleLogout}
+            >
+              <svg className={css.iconLogOutMob} width="20" height="20">
+                <use href={`${sprite}#icon-log-out`}></use>
+              </svg>
+              Log Out
+            </button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default UserMenu;
